bfe68008ffc8b974f87b5e383a6e359e26265b6c
Tof <cristopher.gomez@ug.uchile.cl>
2021-02-11 22:02:55 -0300

    centralizando servidor y cliente

--- deploy/app.js
+++ deploy/app.js
@@ -32,72 +32,66 @@ for (var id in robots) {
     console.log ( "Activated robot: «" + id + "»" );
 }
 
+app.use(express.static('world'));
+
 app.get('/', (req,res) => {
-    res.send('Hello World!')
+    res.sendFile('./world/fgmm_world.html')
 });
 
-var mainServer = http.createServer(function (request, response) {
-    if (request.url == '/'){
+app.post('/',(req,res) => {
+});
+
+app.post('/update',(req,res) => {
+    var content = '';
+    req.on('data', function (data) {
+        content += data;
+    });
+    req.on('end', function () {
+        var values = JSON.parse( content );
+        var updatedValues = {};
         
-    }
-    
-    if (request.method !== 'POST') {
-        response.writeHead(405, { 'Content-Type': 'text/plain' });
-        response.write('Method Not Allowed');
-        response.end();
-        return;
-    }
+        for (var id in robots) {
+            updatedValues[id] = robots[id].update( values[id] );
+        }
+        var text = JSON.stringify( updatedValues );
+        res.writeHead(200, {
+            'Content-Type': 'text/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+            'Access-Control-Max-Age': 2592000,
+            'Server': managerName,
+            'X-Powered-By': 'RoboThree ' + roboThreeRelease,
+            'Content-Length': text.length
+            });
+        res.write(text);
+        res.end();
+    })
 
-    if (request.url == '/update') {
-        var content = '';
-        request.on('data', function (data) {
-            content += data;
-        });
-        request.on('end', function () {
-            var values = JSON.parse( content );
-            var updatedValues = {};
-            
-            for (var id in robots) {
-                updatedValues[id] = robots[id].update( values[id] );
-            }
-            var text = JSON.stringify( updatedValues );
-            response.writeHead(200, {
-                'Content-Type': 'text/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-                'Access-Control-Max-Age': 2592000,
-                'Server': managerName,
-                'X-Powered-By': 'RoboThree ' + roboThreeRelease,
-                'Content-Length': text.length
-                });
-            response.write(text);
-            response.end();
-        })
-    }
-    else if (request.url == '/exec') {
-        var content = '';
-        request.on('data', function (data) {
-            content += data;
-        });
-        request.on('end', function () {
-            var values = JSON.parse( content );
-            console.log ('[manager] values: ');
-            console.log ( values );
-            if ( typeof values.robotId !== 'undefined' ) {
-                console.log('[manager] Execution of ' + values.command + ' called... ');
-                console.log('[manager] Parameters: ');
-                console.log(values.parameters);
-                robots[values.robotId].exec( values.command, values.parameters, response ); // we delegate the response to the robot
-            }
-            else {
-                response.writeHead(404, { 'Content-Type': 'text/plain', 'Server': 'RoboThree Robot\'s Manager' });
-                response.write('Robot Not Found');
-                response.end();
-            }
-        })
-    }
-});
+})
+app.post('/exec',(req,res) => {
+    var content = '';
+    req.on('data', function (data) {
+        content += data;
+    });
+    req.on('end', function () {
+        var values = JSON.parse( content );
+        console.log ('[manager] values: ');
+        console.log ( values );
+        if ( typeof values.robotId !== 'undefined' ) {
+            console.log('[manager] Execution of ' + values.command + ' called... ');
+            console.log('[manager] Parameters: ');
+            console.log(values.parameters);
+            robots[values.robotId].exec( values.command, values.parameters, response ); // we delegate the response to the robot
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'text/plain', 'Server': 'RoboThree Robot\'s Manager' });
+            res.write('Robot Not Found');
+            res.end();
+        }
+    })
+})
+
 
 app.listen(port, () => {
     console.log ( "Listening on port " + port );
-}
+})
