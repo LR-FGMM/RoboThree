c00c252c93f970c116567828504e5cfbdb753b5c
Tof <cristopher.gomez@ug.uchile.cl>
2021-02-11 23:53:32 -0300

    abandonen toda esperanza

--- deploy/fake-server/server.js
+++ deploy/fake-server/server.js
@@ -19,57 +19,57 @@ for (var id in robots) {
     console.log ( "Activated robot: «" + id + "»" );
 }
 
-app.post('/update',(req,res) => {
-    var content = '';
-    req.on('data', function (data) {
-        content += data;
-    });
-    req.on('end', function () {
-        var values = JSON.parse( content );
-        var updatedValues = {};
-        
-        for (var id in robots) {
-            updatedValues[id] = robots[id].update( values[id] );
-        }
-        var text = JSON.stringify( updatedValues );
-        res.writeHead(200, {
-            'Content-Type': 'text/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-            'Access-Control-Max-Age': 2592000,
-            'Server': managerName,
-            'X-Powered-By': 'RoboThree ' + roboThreeRelease,
-            'Content-Length': text.length
+createServer({
+    routes(){
+        this.post('/update',(req,res) => {
+            var content = '';
+            req.on('data', function (data) {
+                content += data;
             });
-        res.write(text);
-        res.end();
-    })
-
+            req.on('end', function () {
+                var values = JSON.parse( content );
+                var updatedValues = {};
+                
+                for (var id in robots) {
+                    updatedValues[id] = robots[id].update( values[id] );
+                }
+                var text = JSON.stringify( updatedValues );
+                res.writeHead(200, {
+                    'Content-Type': 'text/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+                    'Access-Control-Max-Age': 2592000,
+                    'Server': managerName,
+                    'X-Powered-By': 'RoboThree ' + roboThreeRelease,
+                    'Content-Length': text.length
+                    });
+                res.write(text);
+                res.end();
+            })
+        
+        })
+        this.post('/exec',(req,res) => {
+            var content = '';
+            req.on('data', function (data) {
+                content += data;
+            });
+            req.on('end', function () {
+                var values = JSON.parse( content );
+                console.log ('[manager] values: ');
+                console.log ( values );
+                if ( typeof values.robotId !== 'undefined' ) {
+                    console.log('[manager] Execution of ' + values.command + ' called... ');
+                    console.log('[manager] Parameters: ');
+                    console.log(values.parameters);
+                    robots[values.robotId].exec( values.command, values.parameters, response ); // we delegate the response to the robot
+                }
+                else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain', 'Server': 'RoboThree Robot\'s Manager' });
+                    res.write('Robot Not Found');
+                    res.end();
+                }
+            })
+        })
+    }
 })
-app.post('/exec',(req,res) => {
-    var content = '';
-    req.on('data', function (data) {
-        content += data;
-    });
-    req.on('end', function () {
-        var values = JSON.parse( content );
-        console.log ('[manager] values: ');
-        console.log ( values );
-        if ( typeof values.robotId !== 'undefined' ) {
-            console.log('[manager] Execution of ' + values.command + ' called... ');
-            console.log('[manager] Parameters: ');
-            console.log(values.parameters);
-            robots[values.robotId].exec( values.command, values.parameters, response ); // we delegate the response to the robot
-        }
-        else {
-            res.writeHead(404, { 'Content-Type': 'text/plain', 'Server': 'RoboThree Robot\'s Manager' });
-            res.write('Robot Not Found');
-            res.end();
-        }
-    })
-})
 
-
-app.listen(port, () => {
-    console.log ( "Listening on port " + port );
-})
