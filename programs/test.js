9bc94f22a6bb16c45f31c88e90613cce32a4ad8a
Tof <cristopher.gomez@ug.uchile.cl>
2021-02-12 21:03:08 -0300

    pruning

--- programs/test.js
+++
@@ -1,6 +0,0 @@
-var LRCommander = require('LRCommander');
-require('extend')( global, new LRCommander( 'arm', '127.0.0.1:9080' ).functions );
-
-for ( var i = 0; i<3; i++ ) {
-    moveJoint(180);
-}
